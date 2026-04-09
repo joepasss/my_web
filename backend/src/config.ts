3557d121592992666152d9da3519b2b2668f66fb
joepasss@gentoo <farwes1180@gmail.com>
2026-04-10 02:50:36 +0900

    add db init

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,6 +2,14 @@ import dotenv from "dotenv";
 import path from "path";
 dotenv.config();
 
+export const DB_CONFIG = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432"),
+};
+
 export const UPLOAD_ROOT = process.env.UPLOAD_PATH || "";
 export const PORT = process.env.PORT;
 export const SERVER_URL = process.env.SERVER_URL;
--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,19 +1,42 @@
-import pg from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import pg from "pg";
+import fs from "fs";
+import { DB_CONFIG } from "@config";
+import path from "node:path";
 
 const { Pool } = pg;
+export const pool = new Pool(DB_CONFIG);
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || '5432'),
+pool.on("error", (err) => {
+  console.error("DB_ERROR:", err);
 });
 
+export const initDB = async () => {
+  const client = await pool.connect();
+  try {
+    console.log("Checking database tables...");
 
-pool.on('connect', () => {
-  console.log('DB connected');
-});
+    const res = await client.query(`
+                                    SELECT EXISTS (
+                                    SELECT FROM information_schema.tables
+                                    WHERE table_schema = 'public'
+                                    AND table_name = 'photos'
+                                    );
+    `);
+
+    if (!res.rows[0].exists) {
+      console.log("Database is empty. initializing from init.sql...");
+      const sqlPath = path.join(__dirname, "../db/init.sql");
+      const sql = fs.readFileSync(sqlPath, "utf8");
+      await client.query(sql);
+
+      console.log("database initalization completed.");
+    } else {
+      console.log("Database tables already exist. Skipping init.");
+    }
+  } catch (err) {
+    console.error("ERROR during DB initialization:", err);
+    throw err;
+  } finally {
+    client.release();
+  }
+};
